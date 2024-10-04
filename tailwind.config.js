7a8e2482aabcac705ce031dd48dbebca8ed46660
NixNash <naqashkashim558@gmail.com>
2024-10-04 09:06:16 +0500

    intial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'display': ['Poppins', 'sans-sarif'],
+        'body': ['Inter', 'sans-sarif']
+      },
+      colors: {
+        'primary': '#3238f2'
+      }
+    },
+  },
+  plugins: [],
+}
+
